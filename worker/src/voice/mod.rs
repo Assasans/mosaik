66595d496eb9ced6cacdd11d4083fda42b1195f9
Daniil Pryima <swimmin2@gmail.com>
2023-04-10 12:30:47 +0300

    feat(voice): Playing files works

--- worker/src/voice/mod.rs
+++ worker/src/voice/mod.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Error, ErrorKind, Read, Write, Cursor};
+use std::io::{Error, ErrorKind, Read, Write, Cursor, stdout};
 use std::net::{TcpStream, IpAddr};
 use std::str::FromStr;
 use std::time::Duration;
@@ -25,7 +25,7 @@ use symphonia::core::meta::MetadataOptions;
 use symphonia::core::probe::Hint;
 use symphonia::default::get_codecs;
 use tokio::net::{TcpStream as TokioTcpStream, UdpSocket};
-use tokio::time::sleep;
+use tokio::time::{sleep, Instant};
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
 use tokio_tungstenite::WebSocketStream;
@@ -219,236 +219,274 @@ pub async fn connect_voice_gateway(endpoint: &str, guild_id: u64, user_id: u64, 
   };
   println!("Session desciption: {:?}", session_description);
 
-  let mut file = File::open("/home/assasans/Downloads/output.opus")?;
-  // let source = MediaSourceStream::new(Box::new(file), Default::default());
+  let mut file = File::open("/home/assasans/Downloads/output.mp3")?;
+  let source = MediaSourceStream::new(Box::new(file), Default::default());
 
-  // let mut hint = Hint::new();
-  // hint.with_extension("mp3");
+  let mut hint = Hint::new();
+  hint.with_extension("mp3");
 
-  // // Use the default options for metadata and format readers.
-  // let meta_opts: MetadataOptions = Default::default();
-  // let fmt_opts: FormatOptions = Default::default();
+  // Use the default options for metadata and format readers.
+  let meta_opts: MetadataOptions = Default::default();
+  let fmt_opts: FormatOptions = Default::default();
 
-  // // Probe the media source.
-  // let probed = symphonia::default::get_probe()
-  //   .format(&hint, source, &fmt_opts, &meta_opts)
-  //   .expect("unsupported format");
+  // Probe the media source.
+  let probed = symphonia::default::get_probe()
+    .format(&hint, source, &fmt_opts, &meta_opts)
+    .expect("unsupported format");
 
-  // // Get the instantiated format reader.
-  // let mut format = probed.format;
+  // Get the instantiated format reader.
+  let mut format = probed.format;
 
-  // // Find the first audio track with a known (decodeable) codec.
-  // let track = format
-  //   .tracks()
-  //   .iter()
-  //   .find(|it| it.codec_params.codec != CODEC_TYPE_NULL)
-  //   .expect("no supported audio tracks");
+  // Find the first audio track with a known (decodeable) codec.
+  let track = format
+    .tracks()
+    .iter()
+    .find(|it| it.codec_params.codec != CODEC_TYPE_NULL)
+    .expect("no supported audio tracks");
 
-  // // Use the default options for the decoder.
-  // let dec_opts: DecoderOptions = Default::default();
+  // Use the default options for the decoder.
+  let dec_opts: DecoderOptions = Default::default();
 
-  // // Create a decoder for the track.
-  // let mut decoder = get_codecs()
-  //   .make(&track.codec_params, &dec_opts)
-  //   .expect("unsupported codec");
+  // Create a decoder for the track.
+  let mut decoder = get_codecs()
+    .make(&track.codec_params, &dec_opts)
+    .expect("unsupported codec");
 
-  // // Store the track identifier, it will be used to filter packets.
-  // let track_id = track.id;
+  // Store the track identifier, it will be used to filter packets.
+  let track_id = track.id;
 
   let mut sequence = random::<u16>();
   let mut timestamp = random::<u32>();
   let mut nonce = 0;
 
   let mut encoder = Encoder::new(SampleRate::Hz48000, Channels::Stereo, Application::Audio)?;
-  encoder.set_bitrate(Bitrate::BitsPerSecond(96000))?;
+  encoder.set_bitrate(Bitrate::BitsPerSecond(512000))?;
 
   let mut opus_decoder = Decoder::new(SampleRate::Hz48000, Channels::Stereo)?;
 
-  let mut rtp_packet = [0; 12 + 15 + 24 + 16]; // 1460];
+  let mut rtp_packet = [0; 1460];
 
-  // let mut sample_count = 0;
-  // let mut sample_buf = None;
+  let mut sample_count = 0;
+  let mut sample_buf = None;
 
   let key = Key::from_slice(&session_description.secret_key);
   let cipher = XSalsa20Poly1305::new(&key);
   let dcipher = XSalsa20Poly1305::new(&key);
 
-  {
-    let mut buf = [0; MutableKeepalivePacket::minimum_packet_size()];
-    let mut view = MutableKeepalivePacket::new(&mut rtp_packet[..]).unwrap();
-    view.set_ssrc(ready.ssrc);
+  let mut send_time = Instant::now();
+  let mut send_time_2 = Instant::now();
 
-    udp.send(&buf[..]).await?;
-  }
+  // let mut demuxer = Demuxer::new();
+  // loop {
+  //   let mut buf = [0; 8192];
+  //   let size = file.read(&mut buf)?;
+  //   if size == 0 {
+  //     break;
+  //   }
 
-  let mut demuxer = Demuxer::new();
+  //   demuxer.push(&buf)?;
+  // }
+
+  let mut sample_buffer = Vec::new();
+
+  let spin_sleeper = spin_sleep::SpinSleeper::new(100_000)
+    .with_spin_strategy(spin_sleep::SpinStrategy::YieldThread);
+
+  let mut time = Instant::now();
+  let mut deadline = Instant::now();
   loop {
-    let mut buf = [0; 8192];
-    let size = file.read(&mut buf)?;
-    if size == 0 {
-      break;
+    deadline = Instant::now() + Duration::from_millis(1000 / 50 - 1);
+
+    if Instant::now() >= send_time {
+      send_time = Instant::now() + Duration::from_secs(5);
+
+      let mut buf = [0; MutableKeepalivePacket::minimum_packet_size()];
+      let mut view = MutableKeepalivePacket::new(&mut rtp_packet[..]).unwrap();
+      view.set_ssrc(ready.ssrc);
+
+      udp.send(&buf[..]).await?;
+
+      println!("Sent UDP keepalive");
     }
 
-    demuxer.push(&buf)?;
-  }
+    if Instant::now() >= send_time_2 {
+      send_time_2 = Instant::now() + Duration::from_millis(13750);
 
-  loop {
+      let heartbeat_message = Message::Text(serde_json::to_string(&json!({
+        "op": 3,
+        "d": nonce
+      })).unwrap());
+      if let Err(error) = socket.send(heartbeat_message).await {
+        println!("Error: {:?}", error);
+      }
+      if let Err(error) = socket.flush().await {
+        println!("Error: {:?}", error);
+      }
+      println!("Sent WebSocket keepalive");
+
+      println!("ACK: {:?}", socket.next().await.unwrap());
+    }
+
     // Get the next packet from the media format.
-    // let packet = match format.next_packet() {
-    let mut buf = vec![0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe]; // demuxer.next().unwrap();
-    //   Ok(packet) => packet,
-    //   Err(symphonia::core::errors::Error::ResetRequired) => {
-    //     // The track list has been changed. Re-examine it and create a new set of decoders,
-    //     // then restart the decode loop. This is an advanced feature and it is not
-    //     // unreasonable to consider this "the end." As of v0.5.0, the only usage of this is
-    //     // for chained OGG physical streams.
-    //     unimplemented!();
-    //   }
-    //   Err(err) => {
-    //     // A unrecoverable error occured, halt decoding.
-    //     panic!("{}", err);
-    //   }
-    // };
+    let packet = match format.next_packet() {
+    // let mut buf = vec![0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe];
+    // let mut buf = demuxer.next().unwrap();
+      Ok(packet) => packet,
+      Err(symphonia::core::errors::Error::ResetRequired) => {
+        // The track list has been changed. Re-examine it and create a new set of decoders,
+        // then restart the decode loop. This is an advanced feature and it is not
+        // unreasonable to consider this "the end." As of v0.5.0, the only usage of this is
+        // for chained OGG physical streams.
+        unimplemented!();
+      }
+      Err(err) => {
+        // A unrecoverable error occured, halt decoding.
+        panic!("{}", err);
+      }
+    };
 
-    // // Consume any new metadata that has been read since the last packet.
-    // while !format.metadata().is_latest() {
-    //   // Pop the old head of the metadata queue.
-    //   format.metadata().pop();
+    // Consume any new metadata that has been read since the last packet.
+    while !format.metadata().is_latest() {
+      // Pop the old head of the metadata queue.
+      format.metadata().pop();
 
-    //   // Consume the new metadata at the head of the metadata queue.
-    // }
+      // Consume the new metadata at the head of the metadata queue.
+    }
 
-    // // If the packet does not belong to the selected track, skip over it.
-    // if packet.track_id() != track_id {
-    //   continue;
-    // }
+    // If the packet does not belong to the selected track, skip over it.
+    if packet.track_id() != track_id {
+      continue;
+    }
 
-    // // Decode the packet into audio samples.
-    // match decoder.decode(&packet) {
-    //   Ok(buffer) => {
-    //     // If this is the *first* decoded packet, create a sample buffer matching the
-    //     // decoded audio buffer format.
-    //     if sample_buf.is_none() {
-    //       // Get the audio buffer specification.
-    //       let spec = *buffer.spec();
+    // Decode the packet into audio samples.
+    match decoder.decode(&packet) {
+      Ok(buffer) => {
+        // If this is the *first* decoded packet, create a sample buffer matching the
+        // decoded audio buffer format.
+        if sample_buf.is_none() {
+          // Get the audio buffer specification.
+          let spec = *buffer.spec();
 
-    //       // Get the capacity of the decoded buffer. Note: This is capacity, not length!
-    //       let duration = buffer.capacity() as u64;
+          // Get the capacity of the decoded buffer. Note: This is capacity, not length!
+          let duration = buffer.capacity() as u64;
 
-    //       // Create the f32 sample buffer.
-    //       sample_buf = Some(SampleBuffer::<i16>::new(duration, spec));
-    //     }
-
-    //     // Copy the decoded audio buffer into the sample buffer in an interleaved format.
-    //     if let Some(buf) = &mut sample_buf {
-    //       buf.copy_interleaved_ref(buffer);
-
-    //       // The samples may now be access via the `samples()` function.
-    //       sample_count += buf.samples().len();
-    //       println!("Decoded {} samples", sample_count);
-
-          let mut view = MutableRtpPacket::new(&mut rtp_packet[..]).unwrap();
-          view.set_version(2);
-          view.set_payload_type(RtpType::Unassigned(0x78));
-          view.set_sequence(sequence.into());
-          sequence += 1;
-          view.set_timestamp(timestamp.into());
-          timestamp += 100;
-          view.set_ssrc(ready.ssrc);
-
-          let payload = view.payload_mut();
-          let mut plaintext = buf; // Vec::new();
-          // let mut plaintext = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-          let size = std::cmp::min(plaintext.len(), 1460 - TAG_SIZE - 24);
-          // let size = plaintext.len();
-          // plaintext.resize(payload.len() - 4 - TAG_SIZE, 0);
-
-          // println!("samples: {:?}", &buf.samples()[..960 / 4]);
-          // println!("buf: {}", &buf.len());
-
-          // let size = encoder.encode(&buf.samples()[..960 / 4], &mut plaintext)?;
-          // println!("encoded: {:?}", &plaintext[..size]);
-
-          // let mut debik = Vec::<i16>::new();
-          // let mut decoded = MutSignals::try_from(&mut debik[..])?;
-          // let decoded_size = opus_decoder.decode(Some(Packet::try_from(&plaintext[..size])?), decoded, false)?;
-          // println!("decoded ({}): {:?}", decoded_size, &debik[..decoded_size]);
-
-          // let mut nonce_bytes = Vec::new();
-          // nonce_bytes.write_i32::<BigEndian>(nonce)?;
-          // nonce_bytes.write_i32::<BigEndian>(0)?;
-          // nonce_bytes.write_i32::<BigEndian>(0)?;
-          // nonce_bytes.write_i32::<BigEndian>(0)?;
-          // nonce_bytes.write_i32::<BigEndian>(0)?;
-          // nonce_bytes.write_i32::<BigEndian>(0)?;
-
-          let mut nonce_bytes = random::<[u8; 24]>();
-
-          let ct = cipher.encrypt(GenericArray::from_slice(&nonce_bytes.to_vec()), &plaintext[..size])?;
-
-          println!("Payload: {}, plaintext: {}, ct: {}", payload.len(), plaintext.len(), ct.len());
-          let mut enn = Vec::new();
-          {
-            let mut c = Cursor::new(&mut enn);
-            // c.write_all(&plaintext[..size])?;
-            // c.write_all(&tag)?;
-            c.write_all(&ct)?;
-            c.write_all(&nonce_bytes)?;
-            // c.write_i32::<BigEndian>(nonce)?;
-          }
-          nonce += 1;
-          // println!("encrypted ({}): {:?}", plaintext[..size].len(), &plaintext[..size]);
-          // println!("enn ({}): {:?}", enn.len(), enn);
-
-          let decrypted = dcipher.decrypt(GenericArray::from_slice(&nonce_bytes.to_vec()), &enn[..size+TAG_SIZE]).unwrap();
-          // println!("decrypted ({}): {:?}", decrypted.len(), decrypted);
-
-          view.set_payload(&enn);
-
-          println!("rtp_packet: {}", rtp_packet.len());
-          // println!("rtp_packet: {:?}", rtp_packet);
-
-          udp.send(&rtp_packet[..]).await?;
-
-          let heartbeat_message = Message::Text(serde_json::to_string(&json!({
-            "op": 1,
-            "d": 12345
-          })).unwrap());
-          if let Err(error) = socket.send(heartbeat_message).await {
-            println!("Error: {:?}", error);
-          }
-
-          let select_message = Message::Text(serde_json::to_string(&json!({
-            "op": 5,
-            "d": {
-              "speaking": 1,
-              "delay": 0,
-              "ssrc": ready.ssrc
-            }
-          })).unwrap());
-          if let Err(error) = socket.send(select_message).await {
-            println!("Error: {:?}", error);
-          }
-
-          socket.flush().await?;
-
-          thread::sleep(Duration::from_millis(100));
+          // Create the f32 sample buffer.
+          sample_buf = Some(SampleBuffer::<i16>::new(duration, spec));
         }
-      // }
-      // Err(symphonia::core::errors::Error::IoError(_)) => {
-      //   // The packet failed to decode due to an IO error, skip the packet.
-      //   continue;
-      // }
-      // Err(symphonia::core::errors::Error::DecodeError(_)) => {
-      //   // The packet failed to decode due to invalid data, skip the packet.
-      //   continue;
-      // }
-      // Err(err) => {
-      //   // An unrecoverable error occured, halt decoding.
-      //   panic!("{}", err);
-      // }
-    // }
-  // }
+
+        // Copy the decoded audio buffer into the sample buffer in an interleaved format.
+        if let Some(buf) = &mut sample_buf {
+          buf.copy_interleaved_ref(buffer);
+
+          // The samples may now be access via the `samples()` function.
+          sample_count += buf.samples().len();
+          // println!("Decoded {} samples", sample_count);
+
+          sample_buffer.extend_from_slice(buf.samples());
+
+          if sample_buffer.len() >= 1920 {
+            let samples: Vec<i16> = sample_buffer.drain(..1920).collect();
+
+            let mut view = MutableRtpPacket::new(&mut rtp_packet[..]).unwrap();
+            view.set_version(2);
+            view.set_payload_type(RtpType::Unassigned(0x78));
+            view.set_sequence(sequence.into());
+            sequence += 1;
+            view.set_timestamp(timestamp.into());
+            timestamp += 48000 / 50;
+            view.set_ssrc(ready.ssrc);
+
+            let payload = view.payload_mut();
+            let mut plaintext = Vec::new();
+            // let mut plaintext = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            // let size = std::cmp::min(plaintext.len(), 1460 - TAG_SIZE - 24);
+            // let size = plaintext.len();
+            plaintext.resize(payload.len() - 24 - TAG_SIZE, 0);
+
+            // println!("samples: {:?}", &buf.samples()[..960 / 4]);
+            // println!("buf: {}", &buf.len());
+
+            // &buf.samples()[..960]
+            let size = encoder.encode(&samples[..], &mut plaintext)?;
+            // println!("encoded: {:?}", &plaintext[..size]);
+
+            // let mut debik = Vec::<i16>::new();
+            // let mut decoded = MutSignals::try_from(&mut debik[..])?;
+            // let decoded_size = opus_decoder.decode(Some(Packet::try_from(&plaintext[..size])?), decoded, false)?;
+            // println!("decoded ({}): {:?}", decoded_size, &debik[..decoded_size]);
+
+            // let mut nonce_bytes = Vec::new();
+            // nonce_bytes.write_i32::<BigEndian>(nonce)?;
+            // nonce_bytes.write_i32::<BigEndian>(0)?;
+            // nonce_bytes.write_i32::<BigEndian>(0)?;
+            // nonce_bytes.write_i32::<BigEndian>(0)?;
+            // nonce_bytes.write_i32::<BigEndian>(0)?;
+            // nonce_bytes.write_i32::<BigEndian>(0)?;
+
+            let mut nonce_bytes = random::<[u8; 24]>();
+
+            let ct = cipher.encrypt(GenericArray::from_slice(&nonce_bytes.to_vec()), &plaintext[..size])?;
+
+            // println!("Payload: {}, plaintext: {}, ct: {}", payload.len(), plaintext.len(), ct.len());
+            let mut enn = Vec::new();
+            {
+              let mut c = Cursor::new(&mut enn);
+              // c.write_all(&plaintext[..size])?;
+              // c.write_all(&tag)?;
+              c.write_all(&ct)?;
+              c.write_all(&nonce_bytes)?;
+              // c.write_i32::<BigEndian>(nonce)?;
+            }
+            nonce += 1;
+            // println!("encrypted ({}): {:?}", plaintext[..size].len(), &plaintext[..size]);
+            // println!("enn ({}): {:?}", enn.len(), enn);
+
+            // let decrypted = dcipher.decrypt(GenericArray::from_slice(&nonce_bytes.to_vec()), &enn[..size+TAG_SIZE]).unwrap();
+            // println!("decrypted ({}): {:?}", decrypted.len(), decrypted);
+
+            view.set_payload(&enn[..]);
+
+            // println!("rtp_packet: {}", rtp_packet.len());
+            // println!("rtp_packet: {:?}", rtp_packet);
+
+            // thread::sleep(deadline - Instant::now());
+            spin_sleeper.sleep(deadline - Instant::now());
+
+            udp.send(&rtp_packet[..12 + enn.len()]).await?;
+            let mut new_time = Instant::now();
+            if new_time - time > Duration::from_millis(1000 / 50) {
+              println!("Packet deadline violation: {:?}", new_time - time);
+            }
+            time = new_time;
+
+            // let select_message = Message::Text(serde_json::to_string(&json!({
+            //   "op": 5,
+            //   "d": {
+            //     "speaking": 1,
+            //     "delay": 0,
+            //     "ssrc": ready.ssrc
+            //   }
+            // })).unwrap());
+            // if let Err(error) = socket.send(select_message).await {
+            //   println!("Error: {:?}", error);
+            // }
+          }
+        }
+      }
+      Err(symphonia::core::errors::Error::IoError(_)) => {
+        // The packet failed to decode due to an IO error, skip the packet.
+        continue;
+      }
+      Err(symphonia::core::errors::Error::DecodeError(_)) => {
+        // The packet failed to decode due to invalid data, skip the packet.
+        continue;
+      }
+      Err(err) => {
+        // An unrecoverable error occured, halt decoding.
+        panic!("{}", err);
+      }
+    }
+  }
 
   Ok(())
 }
